701099c55315f1ca09b1bda9623b499491ca4b23
Tommy Huang <tommy_huang@aspeedtech.com>
2024-05-16 14:38:44 +0800

    dts: ast26xx: Add i2c global and controller node
    Add i2c global and controller node.
    Add i2c pinctrl amd groups.
    
    Signed-off-by: Tommy Huang <tommy_huang@aspeedtech.com>
    Change-Id: I3ca10334182deec29e5cda45dd982e9b94388276

--- include/zephyr/dt-bindings/pinctrl/ast26xx-pinctrl-groups.h
+++ include/zephyr/dt-bindings/pinctrl/ast26xx-pinctrl-groups.h
@@ -1,4 +1,20 @@
 #ifndef _AST26XX_PINCTRL_GROUPS_H_
 #define _AST26XX_PINCTRL_GROUPS_H_
 #define pinctrl_uart11_default &pin_txd11 &pin_rxd11
+#define pinctrl_i2c0_default &pin_scl1 &pin_sda1
+#define pinctrl_i2c1_default &pin_scl2 &pin_sda2
+#define pinctrl_i2c2_default &pin_scl3 &pin_sda3
+#define pinctrl_i2c3_default &pin_scl4 &pin_sda4
+#define pinctrl_i2c4_default &pin_scl5 &pin_sda5
+#define pinctrl_i2c5_default &pin_scl6 &pin_sda6
+#define pinctrl_i2c6_default &pin_scl7 &pin_sda7
+#define pinctrl_i2c7_default &pin_scl8 &pin_sda8
+#define pinctrl_i2c8_default &pin_scl9 &pin_sda9
+#define pinctrl_i2c9_default &pin_scl10 &pin_sda10
+#define pinctrl_i2c10_default &pin_scl11 &pin_sda11
+#define pinctrl_i2c11_default &pin_scl12 &pin_sda12
+#define pinctrl_i2c12_default &pin_scl13 &pin_sda13
+#define pinctrl_i2c13_default &pin_scl14 &pin_sda14
+#define pinctrl_i2c14_default &pin_scl15 &pin_sda15
+#define pinctrl_i2c15_default &pin_scl16 &pin_sda16
 #endif /* end of "#ifndef _AST26XX_PINCTRL_GROUPS_H_" */
