8c8480376cc26d1ab3134b46c4d323675c6081bc
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2024-05-29 21:51:08 +0800

    spi-nor: Check QE bit in status register 2 before write
    On Winbond flash part, there exists read status
    register-2 command, 35h. Thus, before setting
    QE bit, a readback check can be executed.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I2f02357c9b3a8d8e29359e1be20161c92c511908

--- drivers/flash/spi_nor_multi_dev.c
+++ drivers/flash/spi_nor_multi_dev.c
@@ -680,6 +680,10 @@ static int spi_nor_cf1_bit1_config(const struct device *dev)
 	ret = spi_nor_rdsr2(dev);
 	if (ret < 0)
 		return ret;
+
+	if ((uint8_t)(ret) & BIT(1))
+		return 0;
+
 	sr[1] = (uint8_t)(ret | BIT(1));
 
 	ret = spi_nor_wren(dev);
@@ -703,8 +707,11 @@ static int spi_nor_cf1_bit1_config(const struct device *dev)
 int spi_nor_sr_cr_bit1_config(const struct device *dev)
 {
 	int ret;
+	struct spi_nor_data *data = dev->data;
 	uint8_t sr_cr[2];
-	uint8_t sr_golden;
+
+	if (data->jedec_id[0] == SPI_NOR_MFR_ID_WINBOND)
+		return spi_nor_cf1_bit1_config(dev);
 
 	ret = spi_nor_rdsr(dev);
 	if (ret < 0)
@@ -716,17 +723,6 @@ int spi_nor_sr_cr_bit1_config(const struct device *dev)
 	ret = spi_nor_wr_sr_cr(dev, sr_cr);
 	if (ret)
 		return ret;
-
-	sr_golden = sr_cr[0];
-
-	ret = spi_nor_rdsr(dev);
-	if (ret < 0)
-		return ret;
-
-	if ((uint8_t)ret != sr_golden) {
-		LOG_ERR("Fail to config SR_CR");
-		return -ENOTSUP;
-	}
 
 	return 0;
 }
